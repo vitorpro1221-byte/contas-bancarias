9a5454a8cc96f38190aca9a5b78d26f8a9501493
Vitor <vitorpro1221@gmail.com>
2026-09-09 23:17:33 -0300

    feat: ContaCorrente com limite de cheque especial e historico diferenciado

--- POO.py
+++ POO.py
@@ -1,7 +1,8 @@
 class ContaBancaria:
     def __init__(self, titular, saldo):
-        self.titular = titular
+        self._titular = titular
         self._saldo = saldo
+        self._historico = []
 
     @property
     def saldo(self):
@@ -14,12 +15,29 @@ class ContaBancaria:
         else:
             print(f"Erro Saldo Indisponivel:{self._saldo}")
 
+    @property
+    def titular(self):
+        return self._titular
+
+    @titular.setter
+    def titular(self, titular):
+        if titular.strip():
+            self._titular = titular
+        else:
+            print("Obrigatorio o Nome de um Titular.")
+
+    @property
+    def historico(self):
+        return self._historico
+
     def depositar(self, valor):
          self.saldo = valor + self.saldo
+         self._historico.append(f"Deposito:{valor}")
 
     def sacar(self, valor):
        if self.saldo >= valor:
         self.saldo = self.saldo - valor
+        self._historico.append(f"Saque:{valor}")
        else:
            print("SALDO INSUFICIENTE")
 
@@ -41,20 +59,35 @@ class ContaPoupanca(ContaBancaria):
 
 class ContaCorrente(ContaBancaria):
     def __init__(self, titular, saldo, limite):
-        super().__init__(titular, saldo)
+        super().__init__(titular, saldo, )
         self.limite = limite
 
-    def sacar(self, valor):
-        if self.saldo - valor >= -self.limite:
-            self.saldo -= valor
-            
+    @property
+    def saldo(self):
+        return self._saldo
+
+    @saldo.setter
+    def saldo(self, valor):
+        if valor >= -self.limite:
+            self._saldo = valor
         else:
             print("LIMITE EXCEDIDO")
+
+    def sacar(self, valor):
+        novo_saldo = self._saldo - valor
+        self.saldo = novo_saldo
+        if self._saldo == novo_saldo:
+            if novo_saldo < 0:
+                self._historico.append(f"Saque de Cheque-Especial R${valor}")
+            else:
+                self._historico.append(F"Saque R${novo_saldo}")
+        
     
 class ContaInvestimento(ContaBancaria):
     def __init__(self, titular, saldo, taxa_imposto):
-        super().__init__(titular, saldo)
+        super().__init__(titular, saldo,)
         self.taxa_imposto = taxa_imposto
+        
 
     def sacar(self, valor):
         if self.saldo >= valor + (valor * self.taxa_imposto):
@@ -66,9 +99,10 @@ class ContaInvestimento(ContaBancaria):
          super().mostrar()
          print(self.taxa_imposto)
 
-conta = ContaBancaria("Ana", 1000)
-print(conta.saldo)
-conta.saldo = 2000
-print(conta.saldo)
-conta.saldo = -50
-print(conta.saldo)
+
+
+
+
+conta = ContaCorrente("Ana", 1000, limite=200)
+conta.sacar(1200)
+print(conta.historico)
